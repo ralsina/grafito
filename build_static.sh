faab0ee63a3239aba433df5cf9a50985dd90e111
Roberto Alsina <roberto.alsina@gmail.com>
2025-05-29 15:25:12 -0300

    build: Build demo server for ... demo purposes

--- build_static.sh
+++ build_static.sh
@@ -14,3 +14,8 @@ mv bin/grafito bin/grafito-static-linux-amd64
 docker build . -f Dockerfile.static --platform linux/arm64 -t grafito-builder
 docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" grafito-builder /bin/sh -c "cd /app && rm -rf lib && shards build --static --release && strip bin/grafito"
 mv bin/grafito bin/grafito-static-linux-arm64
+
+# Build fake ARM64 binary
+docker build . -f Dockerfile.static --platform linux/arm64 -t grafito-builder
+docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" grafito-builder /bin/sh -c "cd /app && rm -rf lib && shards build --static -Dfake_journal --release && strip bin/grafito"
+mv bin/grafito bin/grafito-fake-static-linux-arm64
