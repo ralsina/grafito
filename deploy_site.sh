c3bd0ba86b75069f1a842d4e441c5966f6ca9688
Roberto Alsina <roberto.alsina@gmail.com>
2025-05-31 20:15:56 -0300

    chore: dumb deployment script

--- deploy_site.sh
+++ deploy_site.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" grafito-builder \
+    /bin/sh -c "cd /app && shards build --static -Dfake_journal && strip bin/grafito"
+mv bin/grafito bin/grafito-fake-static-linux-arm64
 
 scp bin/grafito-fake-static-linux-arm64 root@rocky:/usr/local/bin/grafito-fake.1
 ssh root@rocky "mv /usr/local/bin/grafito-fake.1 /usr/local/bin/grafito-fake"
