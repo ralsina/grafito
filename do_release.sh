3568456ab6f199a34d87ca92bbb1283e718ca74f
Roberto Alsina <roberto.alsina@gmail.com>
2025-06-02 14:04:10 -0300

    chore: automate AUR release

--- do_release.sh
+++ do_release.sh
@@ -13,3 +13,4 @@ git commit -a -m "bump: Release v$VERSION"
 git tag "v$VERSION"
 git push --tags
 gh release create "v$VERSION" "bin/$PKGNAME-static-linux-amd64" "bin/$PKGNAME-static-linux-arm64" --title "Release v$VERSION" --notes "$(git cliff -l -s all)"
+bash -x do_aur.sh
