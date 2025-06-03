cb1a16fd26be807252a8e66c3080e75e123ab970
Roberto Alsina <roberto.alsina@gmail.com>
2025-06-03 15:17:46 -0300

    docs: more literate output

--- src/timeline.cr
+++ src/timeline.cr
@@ -1,11 +1,18 @@
+# # timeline.cr
+#
+# At work we use DataDog and I always liked how it showed a histogram of
+# **when** all the events you are seeing happened. While not nearly as
+# fancy (I am *not* a billion-dollar corporation) this is sort of the
+# same thing, except not interactive (for now)
+
 require "time"
 require "html"         # For HTML.escape
 require "./journalctl" # For Journalctl::LogEntry type
 
 module Timeline
-  extend self # Allows calling methods like Timeline.generate_frequency_timeline
+  extend self
 
-  # Alias for the data structure representing a point in the frequency timeline.
+  # The data structure representing a point in the frequency timeline.
   alias TimelinePoint = NamedTuple(start_time: Time, count: Int32)
 
   # Generates a timeline of log entry frequencies.
@@ -14,27 +21,35 @@ module Timeline
   # based on the specified interval, counting the number of entries in each bucket.
   #
   # Arguments:
-  #   logs: An array of `Journalctl::LogEntry` objects. It's assumed that each
-  #         entry has a `timestamp` attribute of type `Time`.
-  #   Log entries will be bucketed by the hour.
+  #
+  # * logs: An array of `Journalctl::LogEntry` objects. It's assumed that each
+  #   entry has a `timestamp` attribute of type `Time`. They will be bucketed
+  #   hourly.
   #
   # Returns:
-  #   An array of `TimelinePoint` named tuples, sorted chronologically by `start_time`.
-  #   Each `TimelinePoint` contains the `start_time` of the bucket and the `count`
-  #   of log entries within that bucket.
-  #   Returns an empty array if the input `logs` array is empty.
+  #
+  # An array of `TimelinePoint` named tuples, sorted chronologically by `start_time`.
+  # Each `TimelinePoint` contains the `start_time` of the bucket and the `count`
+  # of log entries within that bucket.
   def generate_frequency_timeline(
     logs : Array(Journalctl::LogEntry),
   ) : Array(TimelinePoint)
+    # Returns an empty array if the input `logs` array is empty.
     return [] of TimelinePoint if logs.empty?
 
-    counts = Hash(Time, Int32).new(0) # Initialize with default value 0 for counts
+    # Initialize with default value 0 for counts
+    counts = Hash(Time, Int32).new(0)
+
+    # Count the number of entries in hourly buckets.
+    # Yes, it should be more flexible. Who cares.
     logs.each do |entry|
       ts = entry.timestamp
-      truncated_ts = ts.at_beginning_of_hour # Truncate to the hour
+      # Truncate to the hour
+      truncated_ts = ts.at_beginning_of_hour
       counts[truncated_ts] += 1
     end
 
+    # And that's the timeline!
     timeline = counts.map { |start_time, count| {start_time: start_time, count: count} }
     timeline.sort_by!(&.[:start_time])
   end
@@ -42,15 +57,17 @@ module Timeline
   # Generates an SVG representation of a timeline.
   #
   # Arguments:
-  #   timeline_data: An array of `TimelinePoint` data to plot.
-  #   width: Total width of the SVG.
-  #   height: Total height of the SVG.
-  #   padding: Uniform padding around the chart area.
-  #   bar_color: Color of the bars.
-  #   font_family: Font family for text elements in the SVG. (No longer used as text elements are removed)
+  #
+  # * timeline_data: An array of `TimelinePoint` data to plot.
+  # * width: Total width of the SVG.
+  # * height: Total height of the SVG.
+  # * padding: Uniform padding around the chart area.
+  # * bar_color: Color of the bars.
+  # * font_family: Font family for text elements in the SVG. (No longer used as text elements are removed)
   #
   # Returns:
-  #   A string containing the SVG markup.
+  #
+  # * A string containing the SVG markup.
   def generate_svg_timeline(
     timeline_data : Array(TimelinePoint),
     width : Int32 = 800,
@@ -61,7 +78,7 @@ module Timeline
   ) : String
     svg = IO::Memory.new
 
-    # Handle empty data case
+    # Handle empty data case by returning a simple SVG
     if timeline_data.empty?
       svg << %(<svg width="#{width}" height="#{height}" xmlns="http://www.w3.org/2000/svg">)
       svg << %(  <text x="#{width / 2}" y="#{height / 2}" class="no-data-text">No data available</text>)
@@ -79,27 +96,32 @@ module Timeline
     max_count = 1.0 if max_count == 0.0 # Avoid division by zero if all counts are 0
 
     num_points = timeline_data.size
-    slot_width = chart_width.to_f / num_points       # Width for each bar + its spacing
-    actual_bar_width = slot_width * 0.8              # Bar takes 80% of the slot
-    bar_margin = (slot_width - actual_bar_width) / 2 # Margin on each side of the bar within its slot
+    # Width for each bar + its spacing
+    slot_width = chart_width.to_f / num_points
+    # Bar takes 80% of the slot
+    actual_bar_width = slot_width * 0.8
+    # Margin on each side of the bar within its slot
+    bar_margin = (slot_width - actual_bar_width) / 2
 
     # SVG header and styles
     svg << %(<svg width="100%" height="#{height}" viewBox="0 0 #{width} #{height}" xmlns="http://www.w3.org/2000/svg">)
     svg << %(  <style>)
     svg << %(    .bar { fill: #{bar_color}; }")
     svg << %(    .bar:hover { opacity: 0.8; }")
-    # .text-label, .y-axis-label, .x-axis-label, .title-label classes and their font-family styles removed
     svg << %(  </style>)
 
     # Bars
     timeline_data.each_with_index do |point, index|
       bar_h = (point[:count].to_f / max_count) * chart_height
-      bar_h = 0.0 if bar_h < 0 # Ensure non-negative height, though counts should be >= 0
+      # Ensure non-negative height, though counts should be >= 0
+      bar_h = 0.0 if bar_h < 0
 
       bar_x = padding + index * slot_width + bar_margin
       bar_y = height - padding - bar_h
 
+      # Draw a bar
       svg << %(  <rect x="#{bar_x.round(2)}" y="#{bar_y.round(2)}" width="#{actual_bar_width.round(2)}" height="#{bar_h.round(2)}" class="bar">)
+      # Tooltip for the bar
       svg << %(    <title>#{HTML.escape(point[:start_time].to_s("%Y-%m-%d %H:%M") + ": " + point[:count].to_s)}</title>) # Tooltip
       svg << %(  </rect>)
     end
