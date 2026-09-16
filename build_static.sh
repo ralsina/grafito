6641513982ba657299018b5772471ebc983f7f85
Roberto Alsina <roberto.alsina@gmail.com>
2026-09-16 09:37:15 -0300

    chore(build): drop -t from docker run so static builds work without a TTY

--- build_static.sh
+++ build_static.sh
@@ -9,10 +9,10 @@ shards install
 
 # Build for AMD64
 docker build . -f Dockerfile.static -t grafito-builder
-docker run -ti --rm -v "$PWD":/app --user="$UID" grafito-builder /bin/sh -c "cd /app && shards build --static --release && strip bin/grafito"
+docker run -i --rm -v "$PWD":/app --user="$UID" grafito-builder /bin/sh -c "cd /app && shards build --static --release && strip bin/grafito"
 mv bin/grafito bin/grafito-static-linux-amd64
 
 # Build for ARM64
 docker build . -f Dockerfile.static --platform linux/arm64 -t grafito-builder
-docker run -ti --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" grafito-builder /bin/sh -c "cd /app && shards build --static --release && strip bin/grafito"
+docker run -i --rm -v "$PWD":/app --platform linux/arm64 --user="$UID" grafito-builder /bin/sh -c "cd /app && shards build --static --release && strip bin/grafito"
 mv bin/grafito bin/grafito-static-linux-arm64
